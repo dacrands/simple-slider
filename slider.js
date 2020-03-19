cd0f055b3242359b93fc8e389cde701673b77b72
da crands <davecrands@gmail.com>
2020-03-19 11:23:37 -0400

    Pass parent elt to Slider

--- slider.js
+++ slider.js
@@ -1,11 +1,13 @@
-const imgs = document.querySelectorAll(".carousel img");
+const parent = document.querySelectorAll('.carousel');
+const imgs = document.querySelectorAll('.carousel img');
 
 class Slider {
-  constructor(imgsNodeList, showTime) {
+  constructor(imgsNodeList, parent, showTime) {    
     this.imgs = imgsNodeList;
     this.showTime = showTime;
-    this.endIndex = imgsNodeList.length - 1;
+    this.parent = parent;
     this.index = 0;
+    this.endIndex = imgsNodeList.length - 1;
     this.timer;
   }
 
@@ -22,27 +24,27 @@ class Slider {
 
   createBtns() {
     for (let i = 0; i < this.imgs.length; i++) {
-      let newBtn = document.createElement("button");
+      let newBtn = document.createElement('button');
       newBtn.value = newBtn.innerText = i;       
-      newBtn.addEventListener("click", e => {
+      newBtn.addEventListener('click', e => {
         this.stop();
         let currImg = this.imgs[e.target.value];
-        currImg.classList.add("show");
+        currImg.classList.add('show');
       });
       document.querySelector('.buttons').appendChild(newBtn);
     }
   }
 
   clearShowImgClass() {
-    for (let img of this.imgs) {
-      img.classList.remove("show");
+    for (let img of this.imgs) {      
+      img.classList.remove('show');           
     }
   }
 
   switchImg() {
     this.clearShowImgClass();
-    let currImg = this.imgs[this.index];
-    currImg.classList.add("show");
+    let currImg = this.imgs[this.index];    
+    currImg.classList.add('show');
     this.index++;
     if (this.index > this.endIndex) {
       this.index = 0;
@@ -50,6 +52,6 @@ class Slider {
   }
 }
 
-const slider = new Slider(imgs, 3000);
+const slider = new Slider(imgs, parent, 4000);
 slider.createBtns();
-slider.start();
+// slider.start();
